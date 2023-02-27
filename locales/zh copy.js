9a0b85ef36c7762c61a9cfbb7015e52afe88d23c
podsnigame <podsnigame@gmail.com>
2023-02-27 05:54:11 +0000

    update

---
+++ locales/zh copy.js
@@ -0,0 +1,137 @@
+const zh = {
+  __COMMAND_ANALYZE_ANALYZE_LABEL: 'Analisis',
+  __COMMAND_ANALYZE_ANALYZE_TEXT: 'Analisis',
+  __COMMAND_ANALYZE_ANALYZE_PROMPT:
+    'Analisislah hal berikut ini dan berikan rinciannya.',
+  __COMMAND_ANALYZE_LITERARILY_LABEL: 'Analisis sastra',
+  __COMMAND_ANALYZE_LITERARILY_TEXT: 'Analisis sastra',
+  __COMMAND_ANALYZE_LITERARILY_PROMPT:
+    'Analisislah hal berikut ini dengan menggunakan perspektif sastra dan berikan rinciannya.',
+  __COMMAND_ANALYZE_MATHEMATICALLY_LABEL: 'Analisis matematis',
+  __COMMAND_ANALYZE_MATHEMATICALLY_TEXT: 'Analisis matematis',
+  __COMMAND_ANALYZE_MATHEMATICALLY_PROMPT:
+    'Analisis hal berikut ini dengan menggunakan perspektif matematis dan berikan rinciannya.',
+  __COMMAND_ANALYZE_NUMEROLOGICALLY_LABEL: '命理學分析',
+  __COMMAND_ANALYZE_NUMEROLOGICALLY_TEXT: '命理學分析',
+  __COMMAND_ANALYZE_NUMEROLOGICALLY_PROMPT:
+    '使用命理學的角度分析以下內容，並給予細節。',
+  __COMMAND_ANALYZE_PHILOSOPHICALLY_LABEL: '哲學分析',
+  __COMMAND_ANALYZE_PHILOSOPHICALLY_TEXT: '哲學分析',
+  __COMMAND_ANALYZE_PHILOSOPHICALLY_PROMPT:
+    '使用哲學的角度分析以下內容，並給予細節。',
+  __COMMAND_ANALYZE_PSYCHOLOGICALLY_LABEL: '心理學分析',
+  __COMMAND_ANALYZE_PSYCHOLOGICALLY_TEXT: '心理學分析',
+  __COMMAND_ANALYZE_PSYCHOLOGICALLY_PROMPT:
+    '使用心理學的角度分析以下內容，並給予細節。',
+  __COMMAND_BOT_ACTIVATE_LABEL: 'Aktifkan balasan otomatis',
+  __COMMAND_BOT_ACTIVATE_TEXT: 'Aktifkan balasan otomatis',
+  __COMMAND_BOT_ACTIVATE_ALIASES: ['Talk', 'Start Talking'],
+  __COMMAND_BOT_ACTIVATE_REPLY: 'Penjawab otomatis diaktifkan',
+  __COMMAND_BOT_CONTINUE_LABEL: 'Lanjutkan',
+  __COMMAND_BOT_CONTINUE_TEXT: 'Lanjutkan',
+  __COMMAND_BOT_CONTINUE_ALIASES: [],
+  __COMMAND_BOT_DEACTIVATE_LABEL: 'Tutup balasan otomatis',
+  __COMMAND_BOT_DEACTIVATE_TEXT: 'Tutup balasan otomatis',
+  __COMMAND_BOT_DEACTIVATE_ALIASES: ['Diam', 'Berhenti bicara'],
+  __COMMAND_BOT_DEACTIVATE_REPLY: 'Balasan Otomatis Tertutup',
+  __COMMAND_BOT_DRAW_LABEL: 'Silakan menggambar',
+  __COMMAND_BOT_DRAW_TEXT: 'Silakan menggambar',
+  __COMMAND_BOT_DRAW_ALIASES: [],
+  __COMMAND_BOT_DRAW_DEMO_LABEL: 'Silakan menggambar',
+  __COMMAND_BOT_DRAW_DEMO_TEXT: 'Silakan menggambar kucing',
+  __COMMAND_BOT_FORGET_LABEL: 'Lupakan',
+  __COMMAND_BOT_FORGET_TEXT: 'Lupakan',
+  __COMMAND_BOT_FORGET_ALIASES: [],
+  __COMMAND_BOT_FORGET_REPLY: 'Lupa',
+  __COMMAND_BOT_RETRY_LABEL: 'Coba lagi',
+  __COMMAND_BOT_RETRY_TEXT: 'Coba lagi',
+  __COMMAND_BOT_RETRY_ALIASES: [],
+  __COMMAND_BOT_SEARCH_LABEL: 'Query',
+  __COMMAND_BOT_SEARCH_TEXT: 'Pertanyaan',
+  __COMMAND_BOT_SEARCH_ALIASES: [],
+  __COMMAND_BOT_SEARCH_DEMO_LABEL: 'Pertanyaan',
+  __COMMAND_BOT_SEARCH_DEMO_TEXT: 'Tanggal kueri',
+  __COMMAND_BOT_SUMMON_DEMO_LABEL: 'Panggilan',
+  __COMMAND_BOT_SUMMON_DEMO_TEXT: 'Apa kabar?',
+  __COMMAND_BOT_TALK_LABEL: 'Silakan bertanya',
+  __COMMAND_BOT_TALK_TEXT: 'Silakan',
+  __COMMAND_BOT_TALK_ALIASES: [],
+  __COMMAND_BOT_TALK_DEMO_LABEL: 'Silakan bertanya',
+  __COMMAND_BOT_TALK_DEMO_TEXT: 'Apa kabar, silakan',
+  __COMMAND_SUM_ADVISE_LABEL: 'Saran',
+  __COMMAND_SUM_ADVISE_TEXT: 'Saran',
+  __COMMAND_SUM_ADVISE_PROMPT:
+    'Rangkumlah yang berikut ini dan berikan saran yang sesuai. ',
+  __COMMAND_SUM_APOLOGIZE_LABEL: 'Permintaan maaf',
+  __COMMAND_SUM_APOLOGIZE_TEXT: 'Permintaan maaf',
+  __COMMAND_SUM_APOLOGIZE_PROMPT:
+    'Untuk meringkas hal-hal berikut dan memberikan permintaan maaf yang tulus.',
+  __COMMAND_SUM_BLAME_LABEL: 'Kecaman',
+  __COMMAND_SUM_BLAME_TEXT: 'Teguran',
+  __COMMAND_SUM_BLAME_PROMPT:
+    'Untuk meringkas hal-hal berikut dan memberikan teguran.',
+  __COMMAND_SUM_COMFORT_LABEL: 'Penghiburan',
+  __COMMAND_SUM_COMFORT_TEXT: 'Penghiburan',
+  __COMMAND_SUM_COMFORT_PROMPT:
+    'Rangkum hal-hal berikut ini dan berikan penghiburan yang hangat. ',
+  __COMMAND_SUM_COMPLAIN_LABEL: 'Mengeluh',
+  __COMMAND_SUM_COMPLAIN_TEXT: 'Mengeluh',
+  __COMMAND_SUM_COMPLAIN_PROMPT:
+    'Rangkum hal-hal berikut ini dan berikan keluhan-keluhan kecil.',
+  __COMMAND_SUM_ENCOURAGE_LABEL: 'Dorongan',
+  __COMMAND_SUM_ENCOURAGE_TEXT: 'Dorongan',
+  __COMMAND_SUM_ENCOURAGE_PROMPT:
+    'Rangkum hal-hal berikut ini dan berikan dorongan yang hangat.',
+  __COMMAND_SUM_LAUGH_LABEL: 'Ejekan',
+  __COMMAND_SUM_LAUGH_TEXT: 'Ejekan',
+  __COMMAND_SUM_LAUGH_PROMPT:
+    'Rangkumlah yang berikut ini dengan ejekan yang kejam.',
+  __COMMAND_SUM_SUM_LABEL: 'Rangkum',
+  __COMMAND_SUM_SUM_TEXT: 'Rangkum',
+  __COMMAND_SUM_SUM_PROMPT: 'Rangkum yang berikut ini dan berikan rinciannya. ',
+  __COMMAND_SYS_COMMAND_LABEL: 'Instruksi',
+  __COMMAND_SYS_COMMAND_TEXT: 'Instruksi',
+  __COMMAND_SYS_DOC_LABEL: 'Lihat file',
+  __COMMAND_SYS_DOC_TEXT: 'File',
+  __COMMAND_SYS_DEPLOY_LABEL: 'Restart',
+  __COMMAND_SYS_DEPLOY_TEXT: 'Restart',
+  __COMMAND_SYS_DEPLOY_REPLY: 'Mulai ulang',
+  __COMMAND_SYS_REPORT_LABEL: 'Melaporkan masalah',
+  __COMMAND_SYS_REPORT_TEXT: 'Melaporkan masalah',
+  __COMMAND_SYS_REPORT_REPLY:
+    'Jika Anda memiliki pertanyaan, silakan hubungi pengembang di bangundwir@gmail.com, terima kasih.',
+  __COMMAND_SYS_VERSION_LABEL: 'Periksa pembaruan',
+  __COMMAND_SYS_VERSION_TEXT: 'Versi',
+  __COMMAND_SYS_VERSION_REPLY: (version, isLatest) =>
+    `Versi saat ini adalah ${version}${
+      isLatest ? '，telah diperbarui ke versi terbaru' : ''
+    }。`,
+  __COMMAND_TRANSLATE_TO_EN_LABEL: 'Terjemahkan ke bahasa Inggris',
+  __COMMAND_TRANSLATE_TO_EN_TEXT: 'Terjemahkan ke bahasa Inggris',
+  __COMMAND_TRANSLATE_TO_EN_PROMPT:
+    'Terjemahkan yang berikut ini ke dalam bahasa Inggris',
+  __COMMAND_TRANSLATE_TO_JA_LABEL: 'Terjemahkan ke dalam bahasa Jepang',
+  __COMMAND_TRANSLATE_TO_JA_TEXT: 'Terjemahkan ke dalam bahasa Jepang',
+  __COMMAND_TRANSLATE_TO_JA_PROMPT:
+    'Terjemahkan yang berikut ini ke dalam bahasa Jepang',
+  __COMPLETION_DEFAULT_HUMAN_GREETING: (name) =>
+    name ? `Saya adalah ${name}。` : 'Hallo！',
+  __COMPLETION_DEFAULT_AI_GREETING: (name) =>
+    name ? `Saya adalah ${name}。` : 'Hallo！',
+  __COMPLETION_DEFAULT_AI_TONE: (tone) =>
+    tone ? `Tanggapi saya dengan ${tone}：` : '',
+  __COMPLETION_SEARCH: (a, q) =>
+    `Menurut hasil kueri 「${a}」jawab pertanyaan「${q}」`,
+  __COMPLETION_SEARCH_NOT_FOUND: 'Tidak ada data yang ditemukan',
+  __COMPLETION_QUOTATION_MARK_OPENING: '「',
+  __COMPLETION_QUOTATION_MARK_CLOSING: '」',
+  __ERROR_ECONNABORTED: 'Masalah ini terlalu rumit',
+  __ERROR_MAX_GROUPS_REACHED: 'Jumlah grup telah mencapai batas maksimum',
+  __ERROR_MAX_USERS_REACHED: 'Jumlah maksimum pengguna telah tercapai',
+  __ERROR_MISSING_ENV: (v) => `Variabel lingkungan yang hilang：${v}`,
+  __MESSAGE_NEW_VERSION_AVAILABLE: (version) =>
+    `Versi terbaru adalah ${version}，silakan perbarui dari GitHub. `,
+  __USER_DISPLAY_NAME_SOMEONE: 'Seseorang',
+};
+
+export default zh;
